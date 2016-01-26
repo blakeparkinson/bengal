a0d461064d4c4cd18239423307b3d7746c4a289d
blakeparkinson <wizardplow@gmail.com>
2016-01-26 00:41:31 -0700

    testing slick more

--- src/es6/controllers/hw.ctrl.js
+++ src/es6/controllers/hw.ctrl.js
@@ -1,6 +1,10 @@
 'use strict';
 (function() {
   starter.controllers.controller('HwCtrl', function($scope, $stateParams) {
-    console.log('hi');
+    $scope.a = [];
+    for (let j = 0; j < 15; j++){
+      $scope.a.push(j);
+    }
+    console.log($scope.a);
   });
 })();
