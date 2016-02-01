3292ce48fcb1a34998a0041b65a21c3ba1ff94d4
blakeparkinson <wizardplow@gmail.com>
2016-02-01 00:44:05 -0700

    theme-ing + other silliness

--- src/es6/controllers/hw.ctrl.js
+++ src/es6/controllers/hw.ctrl.js
@@ -5,7 +5,31 @@
     for (let j = 0; j < 15; j++){
       $scope.a.push(j);
     }
-    bleService.initializeBLE();
+
+    $scope.knobData = [
+		{
+			value: 30,
+			options: {
+				width: 100,
+				displayInput: false
+			}
+		}
+	];
+
+    $scope.data = 20;
+    $scope.options = {
+        width: 75,
+        fgColor: "#ffec03",
+        skin: "tron",
+        thickness: 0.2,
+        displayPrevious: true
+    };
+
+	$scope.formatOptions = function(data) {
+		data.formattedOptions = JSON.stringify(data.options).replace(/,/g,"\n");
+		return data;
+	};
+    //bleService.initializeBLE();
 
 
   });
