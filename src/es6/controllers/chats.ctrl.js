865bd33620b256bc5d0657b917c4497c8bd6cf83
blakeparkinson <wizardplow@gmail.com>
2016-02-17 11:41:12 -0700

    Adding localStorage service + localStorage example

--- src/es6/controllers/chats.ctrl.js
+++ src/es6/controllers/chats.ctrl.js
@@ -1,12 +1,22 @@
 'use strict';
 (function() {
 
-  starter.controllers.controller('ChatsCtrl', function() {
+  starter.controllers.controller('ChatsCtrl', function($localStorage) {
 
+     $localStorage.set('name', 'Max');
     this.data = {
       text : 'hello world'
 
     };
+
+    $localStorage.setObject('post', {
+    name: 'Thoughts',
+    text: 'Today was a good day'
+  });
+
+  var post = $localStorage.getObject('post');
+
+  console.log(post);
 
   });
 })();
