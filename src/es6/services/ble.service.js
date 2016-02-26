5484dec09525d229a865852643ffee6d02d6ca8c
Blake Parkinson <wizardplow@gmail.com>
2016-02-26 11:08:38 -0700

    ble service items

--- src/es6/services/ble.service.js
+++ src/es6/services/ble.service.js
@@ -1,34 +1,130 @@
-'use strict';
+(function() {
+  'use strict';
+  pulse.services.factory('BLE', function($rootScope, $q, $localStorage, $timeout) {
 
-(function () {
+  var status = 'sss';
 
-  starter.services.factory('bleService', function ($q) {
-    return {
-      initializeBLE: function initializeBLE() {
-        var defer = $q.defer();
-        if (window.cordova) {
-          console.log('ble time');
-          ble.isEnabled(function () {
-            console.log(Date.now() + 'Scanning.');
-            ble.startScan([], function(data){
-              defer.resolve(data);
-            });
-          }, function () {
-            defer.resolve('wah3');
+  var bleITems =  {
+
+  status: status,
+
+
+  scanBLE: function(){
+      var that = this;
+      var deferred = $q.defer();
+      if (window.cordova) {
+
+        //that.devices.length = 0;
+
+        if (connected) {
+          var id = connected.id;
+          ble.disconnect(connected.id, function() {
+            console.log("Disconnected " + id);
           });
-        } else {
-           defer.resolve(this.noCordova());
+          connected = null;
         }
-        return defer.promise;
-      },
-      onDiscover: function onDiscover(data) {
-        console.log(data);
-        return data;
-      },
-      noCordova: function noCordova() {
-        console.log('cordova is not active');
-        return 'wah2';
+
+        ble.startScan([], /* scan for all services */
+          function(peripheral) {
+            if (peripheral) {
+              that.devices.push(peripheral);
+              deferred.resolve();
+            }
+          },
+          function(error) {
+            deferred.reject(error);
+          });
+
+        // stop scan after 5 seconds
+        setTimeout(ble.stopScan, 5000,
+          function() {
+            deferred.resolve();
+          },
+          function() {
+            console.log("stopScan failed");
+            deferred.reject("Error stopping scan");
+          }
+        );
+      } else {
+        deferred.resolve(bleITems.noCordova());
       }
+
+      return deferred.promise;
+    },
+
+    initBLE: function(){
+      status = {'icon': 'ion-camera', 'status': 'scanning', 'fff': 'moo'};
+      console.log(status);
+      bleITems.scanBLE().then((data) => {
+        //success callback
+        if (this.devices.length) {
+          _.forEach(this.devices, (device) => {
+            if (device.name == 'Pulse') {
+
+              bleITems.connectBLE(device.id).then((connectedDevice) => {
+                //status = {'icon': 'ion-video', 'status': 'connected'};
+                console.log('successfully added device');
+              }, (error) => {
+                //status = 'deviceFound';
+                console.log(error);
+              });
+            }
+
+          });
+        }
+      }, (error) => {
+        //failure callback
+        //status = {'icon': 'ion-camera', 'status': 'noDeviceFound'};
+        console.log(error);
+      });
+    },
+
+    connectBLE: function(){
+      var deferred = $q.defer();
+
+      ble.connect(deviceId,
+        function(peripheral) {
+          connected = peripheral;
+          $localStorage.setObject('pulse', peripheral);
+          deferred.resolve(peripheral);
+        },
+        function(error) {
+          deferred.reject(error);
+        }
+      );
+
+      return deferred.promise;
+    },
+
+    noCordova: function() {
+      var deferred = $q.defer();
+      deferred.reject('cordova is not active');
+      return deferred.promise;
+    }
+  };
+
+    return {
+
+
+      devices: [],
+
+      status: bleITems.status,
+
+      scan: function() {
+        bleITems.scanBLE();
+      },
+      connect: function(deviceId) {
+        bleITems.connectBLE();
+      },
+
+      /**
+       * function scanAndConnect - scans BLE devices and connects to the one named Pulse
+       * @return {promise}
+       */
+      scanAndConnect: function() {
+        bleITems.initBLE();
+      }
+
     };
   });
 })();
